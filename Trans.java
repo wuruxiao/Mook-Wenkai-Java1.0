d4c80265217400f7a57259e12bf3644a859dfa6e
wuruxiao <64072344+wuruxiao@users.noreply.github.com>
2021-03-08 17:30:49 +0800

    Add first time
    by WuRuxiao

---
+++ Trans.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int f = in.nextInt();
+		System.out.println((int)((f-32) * 5.0 / 9.0));
+	}
+
+}
